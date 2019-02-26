a509bf8f21a2778ea3c05d9899b8988ed2814ec6
diirector <jozef.kubikml@gmail.com>
2019-02-26 22:52:14 +0100

    Only bullshits was added.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
 
     public static void main(String[] args) {        //velmi solidne
         System.out.println("Hello GitHub, I am here!!");
+        System.out.println("Hello GitHubadasdsa!!");
+        System.out.println("Hello Gidasdsdasdsa!!");
     }
 }
